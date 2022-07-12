c4fe0d55ed43f854532d8056be81ad57c7f556f8
Phil Miler <phill.c.miller@gmail.com>
2022-07-12 18:15:37 -0500

    build

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+import * as React from "react";
+export declare function ReactTerminal(props: any): any;
+export declare const TerminalContextProvider: (props: any) => JSX.Element;
+declare const _default: {
+    ReactTerminal: typeof ReactTerminal;
+    TerminalContextProvider: (props: any) => JSX.Element;
+    TerminalContext: React.Context<any>;
+};
+export default _default;
