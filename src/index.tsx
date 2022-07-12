dafef3172351f6dd2c9abe07a986d79615a7644a
Phil Miler <phill.c.miller@gmail.com>
2022-07-12 17:56:06 -0500

    exported the goddamn terminal context

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as Terminal from "./components/Terminal";
 import * as ContextProvider from "./contexts";
-import { TerminalContextProvider as _TerminalContextProvider } from "./contexts/TerminalContext";
+import { TerminalContextProvider as _TerminalContextProvider, TerminalContext } from "./contexts/TerminalContext";
 
 export function ReactTerminal(props: any): any {
   return (
@@ -15,5 +15,6 @@ export const TerminalContextProvider = _TerminalContextProvider;
 
 export default {
   ReactTerminal,
-  TerminalContextProvider
+  TerminalContextProvider,
+  TerminalContext // THIS IS LITERALLY ALL I WANT. WHY IS IT NOT EXPORTED AHHHHHH
 };
